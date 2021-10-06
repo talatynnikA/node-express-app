67de39dc058149d745e2b66d593c4f752279dcfe
Artyom_Talatynnik <artsyomtalatynnik@gmail.com>
2021-10-06 10:41:17 +0300

    new app

---
+++ index.js
@@ -0,0 +1,38 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
+const {application} = require("express");
+const PORT = process.env.port || 3000
+
+const app = express()
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+app.use(todoRoutes)
+app.use(express.urlencoded({extended: true}))
+
+async function start() {
+    try{
+
+     await mongoose.connect('mongodb+srv://Admin:1qaz2wsx@cluster0.pz98y.mongodb.net/todos',{
+
+     } )
+
+        console.log('koro4 conneht')
+    }
+    catch (e){
+        console.log(e)
+    }
+}
+app.listen(PORT, () =>{
+    console.log('server has been started...')
+})
+
+start()
